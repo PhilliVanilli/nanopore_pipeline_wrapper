3dcef30c239f6332d4fed928e60e25809171aa6a
colin <colin.anthony001@gmail.com>
2020-04-11 18:28:15 +0200

    updated plot title and axis settings

--- src/plot_vcf_qual_depth.py
+++ src/plot_vcf_qual_depth.py
@@ -16,7 +16,7 @@ def plot_depth(x_vals, depth_vals, sample_name, outfile):
     fig, ax = plt.subplots()
     ax.set_ylabel('Sequencing depth')
     ax.set_xlabel('Sequence position')
-    ax.set_title(sample_name)
+    ax.set_title(sample_name + " sequencing depth")
 
     x_labs = []
     last_used_index = 0
@@ -32,9 +32,9 @@ def plot_depth(x_vals, depth_vals, sample_name, outfile):
     # x_labs = list(range(0, len(x_vals), 400))
     ax.set_xticks(x_labs)
     ax.tick_params(axis='x', rotation=90)
-    # ax.xticks()
+
     plt.plot(x_vals, depth_vals)
-    # plt.show()
+
     w = 8
     h = 4
     f = plt.gcf()
@@ -48,9 +48,26 @@ def plot_qual(x_vals, qual_vals, sample_name, outfile):
     fig, ax = plt.subplots()
     ax.set_ylabel('Sequencing quality')
     ax.set_xlabel('Sequence position')
-    ax.set_title(sample_name)
+    ax.set_title(sample_name + " Quality scores")
+    x_labs = []
+    last_used_index = 0
+    for i, val in enumerate(qual_vals):
+        if i == 0:
+            x_labs.append(x_vals[i])
+        elif i < len(qual_vals) - 1:
+            if i < last_used_index + 400:
+                continue
+            elif qual_vals[i + 1] < (val / 3) or qual_vals[i + 1] > (val / 3):
+                last_used_index = i
+                x_labs.append(x_vals[i])
+    ax.set_xticks(x_labs)
+    ax.tick_params(axis='x', rotation=90)
 
     plt.plot(x_vals, qual_vals)
+    w = 8
+    h = 4
+    f = plt.gcf()
+    f.set_size_inches(w, h)
 
     plt.savefig(outfile, ext="png", dpi=300, facecolor="white", bbox_inches="tight")
 
@@ -64,7 +81,6 @@ def main(infile, outpath):
     depth_outfile = pathlib.Path(outpath, sample_name + "_vcf_depth_plot.png")
     qual_outfile = pathlib.Path(outpath, sample_name + "_vcf_qual_plot.png")
     data = pd.read_csv(infile, sep=',', header=0, parse_dates=True)
-    print(data.head(n=4))
 
     plot_depth(data['position'], data['seq_depth'], sample_name, depth_outfile)
     plot_qual(data['position'], data['seq_depth'], sample_name, qual_outfile)
